2d3711bf4e3a1fd72f7fea013c907b394b742faf
Lucas Nahuel Sandoval <lukaz945@gmail.com>
2022-03-24 05:36:03 -0300

    deleting unnecessary hooks

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,6 +1,3 @@
 export { useFormatTime } from './useFormatTime';
 export { useRunTimer } from './useRunTimer';
 export { useSetTitle } from './useSetTitle';
-export { useTheme } from './useTheme';
-export { useTimer } from './useTimer';
-export { useVisibility } from './useVisibility';
